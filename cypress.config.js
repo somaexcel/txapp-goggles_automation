743ed625ee9f778ab8472244dd3865216bc8d0ff
Soma Samuel <soma.excel@gmail.com>
2026-02-03 14:13:44 -0600

    Test with mocha report and screenshot

---
+++ cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    supportFile: "cypress/support/e2e.js",
+    screenshotOnRunFailure: true,
+    video: false,
+    specPattern: "cypress/e2e/**/*.cy.js",
+
+    setupNodeEvents(on, config) {
+      // 🔥 THIS LINE IS REQUIRED
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    },
+  },
+
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    reportFilename: "index",
+    overwrite: false,
+    html: true,
+    json: true,
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
+
+  screenshotsFolder: "cypress/screenshots",
+});
